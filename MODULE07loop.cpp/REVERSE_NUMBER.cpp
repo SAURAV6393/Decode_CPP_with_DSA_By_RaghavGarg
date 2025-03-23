07935a2aaf8dcd005c70babb8b0c183eed835b3d
Saurav Rajput <sauravrajput9839@gmail.com>
2025-03-23 20:38:26 +0530

    module

--- MODULE07loop.cpp/REVERSE_NUMBER.cpp
+++ MODULE07loop.cpp/REVERSE_NUMBER.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
-using namespce std;
+using namespace std;
 int main()
 {
-    
+   // 9874
+
+//    steps
+//    1) last digit
+//    2) r = r*10
+//    3) r += last digit
+//    3) n /= 10 
+   int n ;
+   cout<<"Enter a number : ";
+   cin>>n;
+   int last_digit = 0;
+   int reverse = 0;
+   while(n>0){
+      last_digit = n % 10;
+      reverse *= 10;
+      reverse += last_digit;
+      n/= 10;
+    }
+    cout<<reverse;
 }
