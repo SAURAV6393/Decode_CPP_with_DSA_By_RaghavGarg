0a087516865aa27561633c3f22b11fcdc8194bb5
Saurav Rajput <sauravrajput9839@gmail.com>
2025-04-03 14:58:57 +0530

    add question

---
+++ MODULE09functions.cpp/square_root.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter n ";
+    cin>>n;
+    int m = sqrt(n);
+    cout<<m<<endl;
+    cout<<n*m<<endl;
+}
