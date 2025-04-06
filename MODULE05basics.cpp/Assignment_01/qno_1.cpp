6a656361e7d3e531eda10fc41cb7ed5d62143fc5
Saurav Rajput <sauravrajput9839@gmail.com>
2025-04-06 10:35:02 +0530

    add assignment

---
+++ MODULE05basics.cpp/Assignment_01/qno_1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     cout<<"hello"<<"\n"<<"rajput"<<endl;
+     cout<<"saurav"<<endl<<"world!";
+}
---
+++ MODULE05basics.cpp/Assignment_01/qno_5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int r;
+    cout<<"Enter radius : ";
+    cin>>r;
+    cout<<"circumference of a circle is "<<(2*M_PI*r);
+}
