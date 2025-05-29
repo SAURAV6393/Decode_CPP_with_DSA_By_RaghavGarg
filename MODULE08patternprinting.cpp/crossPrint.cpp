c66bd91a7fabc769149e2c9d1493bb3e5e5e27cf
Saurav Rajput <sauravrajput9839@gmail.com>
2025-05-29 13:09:20 +0530

    add

--- MODULE08patternprinting.cpp/crossPrint.cpp
+++ MODULE08patternprinting.cpp/crossPrint.cpp
@@ -9,7 +9,7 @@ using namespace std;
         for(int j = 1; j<=n ; j++){
             if(i==j || i==n+1-j)
             cout<<" * ";
-            else cout<<" # ";
+            else cout<<"   ";
         }
         cout<<endl;
     }
---
+++ MODULE08patternprinting.cpp/revision/FlippedStar.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+int main() {
+    int n;
+    cout << "Enter the size of the square: ";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        bool flag = true;
+        for(int j = 1; j <= n ; j++){
+           if(i%2==0){
+            cout<<
+           }
+        }
+        cout<<endl;
+    }
+  
+    return 0 ;
+}
