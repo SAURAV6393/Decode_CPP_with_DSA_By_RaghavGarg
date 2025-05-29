d449493458faae3c9de7b7cd6dadce21ed284103
Saurav Rajput <sauravrajput9839@gmail.com>
2025-05-29 20:25:23 +0530

    add

--- MODULE08patternprinting.cpp/revision/FlippedStar.cpp
+++ MODULE08patternprinting.cpp/revision/FlippedStar.cpp
@@ -1,19 +1,17 @@
 #include<iostream>
 using namespace std;
-// 1
-// 0 1
-// 1 0 1
-// 0 1 0 1
+//       *
+//     * *
+//   * * * 
+// * * * *
 int main() {
     int n;
     cout << "Enter the size of the square: ";
     cin >> n;
     for(int i = 1; i <= n; i++){
-        bool flag = true;
         for(int j = 1; j <= n ; j++){
-           if(i%2==0){
-            cout<<
-           }
+           if((i+j)<=n) cout<<"   ";
+          else  cout<<" * ";
         }
         cout<<endl;
     }
--- MODULE08patternprinting.cpp/revision/binaryTriangle.cpp
+++ MODULE08patternprinting.cpp/revision/binaryTriangle.cpp
@@ -8,10 +8,21 @@ int main() {
     int n;
     cout << "Enter the size of the square: ";
     cin >> n;
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i ; j++){
-           if((i+j)%2==0) cout<<1<<" ";
-           else cout<<0<<" ";
+    // for(int i = 1; i <= n; i++){
+    //     for(int j = 1; j <= i ; j++){
+    //        if((i+j)%2==0) cout<<1<<" ";
+    //        else cout<<0<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    int k ;
+    for(int i = 1; i <= n ; i++){
+        if(i%2==0) k = 0;
+        else k = 1;
+        for(int  j = 1 ; j <= i ; j++){
+            cout<<k<<" ";
+            if(k==1) k = 0;
+            else k = 1;
         }
         cout<<endl;
     }
